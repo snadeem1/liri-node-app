77d95ef398f6c94f741402d978c24be442d58d7c
Sitwat <sitwat.nadeem1@gmail.com>
2018-07-17 20:23:36 -0400

    tweet and song working

--- liri.js
+++ liri.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 var Twitter = require("twitter");
 var keys = require("./keys.js");
-//var Spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 var fs = require("fs");
 var request = require("request");
 
 
-//var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var liriOutput = process.argv[2];
@@ -14,6 +14,10 @@ var liriOutput = process.argv[2];
 switch (liriOutput) {
   case "my-tweets":
       myTweets();
+      break;
+
+  case "spotify-this-song":
+      spotifyThisSong();
       break;
 };
 
@@ -31,4 +35,34 @@ switch (liriOutput) {
             };
         });
     };
-  
+
+    function spotifyThisSong(songName){
+        var songName = process.argv[3];
+        if (!songName){
+            songName= "The Sign";
+        };
+        songRequest = songName;
+        spotify.search({
+        type: "track",
+        query: songRequest
+    },
+    function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+          
+        }
+
+        var songs = data.tracks.items;
+
+        for (var i = 0; i < songs.length; i++) {
+      
+          console.log("Artist: " + songs[i].artists[0].name);
+          console.log("song name: " + songs[i].name);
+          console.log ("Preview Url: " + songs[i].preview_url);
+          console.log ("Album Name: " + songs[i].album.name);
+      
+        }
+      
+      });
+
+    };
